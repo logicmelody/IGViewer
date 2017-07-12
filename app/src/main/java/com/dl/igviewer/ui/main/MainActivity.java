cca1cbb6926e27dfb6bb195d5489193a05963ab2
logicmelody <logicmelody@gmail.com>
2017-07-12 16:40:40 +0800

    Refactor package structure

--- app/src/main/java/com/dl/igviewer/ui/main/MainActivity.java
+++ app/src/main/java/com/dl/igviewer/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dl.igviewer;
+package com.dl.igviewer.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dl.igviewer.R;
 
 public class MainActivity extends AppCompatActivity {
 
