0f7c32edb78e55a4baafc0904c3419584ce3f006
logicmelody <logicmelody@gmail.com>
2017-07-12 22:19:58 +0800

    Logo title on action bar in main page

--- app/src/main/java/com/dl/igviewer/ui/main/MainActivity.java
+++ app/src/main/java/com/dl/igviewer/ui/main/MainActivity.java
@@ -1,18 +1,45 @@
 package com.dl.igviewer.ui.main;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 
 import com.dl.igviewer.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
 
-        Log.d("danny", "Token = " + InstagramDataCache.getTokenFromSharedPreference(this));
+    private void initialize() {
+        findViews();
+        setupActionBar();
+        setupViews();
+    }
+
+    private void findViews() {
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+    }
+
+    private void setupActionBar() {
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    private void setupViews() {
+
     }
 }
